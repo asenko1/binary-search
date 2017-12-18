e6d7e837e9c430dabb6f5a87951e81b99dde6842
asenko1 <alex.senko598@gmail.com>
2017-12-18 09:14:24 -0800

    Working solution

--- src/main/BinarySearch.java
+++ src/main/BinarySearch.java
@@ -3,15 +3,27 @@ package main;
 public class BinarySearch {
 	public static int arrayBinarySearch(int[] array, int min, int max, int target){
 		int index = -1;
+		
+		//Error handling
 		if(max < min)
 			return index;
+		else if(array == null)
+			return index;
+		else if(max > array.length)
+			return index;
+		else if(min < 0)
+			return index;
+		
+		//Search
 		int midpoint = (min + max) / 2;
-		if(target == midpoint)
+		if(midpoint >= array.length)
+			return index;
+		else if(target == array[midpoint])
 			return midpoint;
-		else if(target < midpoint){
+		else if(target < array[midpoint]){
 			index = arrayBinarySearch(array, min, midpoint - 1, target);
 		}
-		else if(target > midpoint){
+		else if(target > array[midpoint]){
 			index = arrayBinarySearch(array, midpoint + 1, max, target);
 		}
 		return index;
